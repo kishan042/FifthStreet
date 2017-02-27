18ba8fb05999237e2d8498bcd2524b6c9eda88e0
kishan <kishan042@hotmail.co.uk>
2017-02-27 20:48:27 +0000

    General update test#11
    - creation of new modules for mens and womens page using flex-box

--- men.php
+++ men.php
@@ -31,19 +31,120 @@ include_once 'INC/Config.php';
 
 		include (ROOT_PATH . 'INC/Navbar.php'); 
 
-// Hero-half-plain
-		// copy for H1
-		$h1 = "FOR MEN";
-		//Copy for description
-		$description = "";
+// Hero-half
+        //IMG URL
+        $url = "https://images.unsplash.com/photo-1476147578954-fffd6bf00ab0?dpr=1&auto=format&fit=crop&w=1500&h=1000&q=80&cs=tinysrgb&crop="; 
+        // Amount of tint on image
+        $tint = "tint-1";
+        // copy for H1
+        $h1 = " ";
 
-		include (ROOT_PATH . 'INC/Hero-half-plain.php'); 
+        include (ROOT_PATH . 'INC/Hero-half.php');
 
 // Spacing	
 		// Add a class to hide the seperation
 		//$hide = "hidden-xs-down";
 
+		//include (ROOT_PATH . 'INC/Spacing-mt-50.php');
+
+
+// Blocks-two-compact-extend
+
+		// Background colour class for text
+		$BlockBG = "bg-gray-lightest";
+		// Title
+		$BlockTitle = "FOR MEN";
+		// Copy
+		$BlockText = "Wander across a range of brand collections and save any product which inspires you. Why stop there, you can even go to any physical store.";
+		// type 'hide' to remove the button
+		$hideCTA = "";
+		// Link for CTA
+		$BlockLink = "wardrobe.php";
+		// CTA copy
+		$BlockCTA = "View Items";
+		// Image 1 URL 
+		$BlockIMG_1 = "https://images.unsplash.com/photo-1483140900273-56364d20ddc5?dpr=2&auto=format&fit=crop&w=1500&h=1000&q=80&cs=tinysrgb&crop=";
+		// Image 2 URL 
+		$BlockIMG_2 = "https://images.unsplash.com/photo-1483140900273-56364d20ddc5?dpr=2&auto=format&fit=crop&w=1500&h=1000&q=80&cs=tinysrgb&crop=";
+		// Image 3 URL 
+		$BlockIMG_3 = "https://images.unsplash.com/photo-1483140900273-56364d20ddc5?dpr=2&auto=format&fit=crop&w=1500&h=1000&q=80&cs=tinysrgb&crop=";
+
+		include (ROOT_PATH . 'INC/Blocks-two-compact-extend.php');
+
+
+?>
+
+
+
+<!-- <div class="container-fluid bg-gray-lightest hidden-sm-down">
+	<div class="row py-3 flex-center">
+	    <div id="product-img" class="col-md-3 col-lg-3 col-xxl-2" style="background-image: url(<?php echo $BlockIMG_1 ?>)">
+	    </div>
+	    <div id="product-img" class="col-md-3 col-lg-3 col-xxl-2" style="background-image: url(<?php echo $BlockIMG_2 ?>)">
+	    </div>
+	    <div id="product-img" class="col-md-3 col-lg-3 col-xxl-2" style="background-image: url(<?php echo $BlockIMG_3 ?>)">
+	    </div>
+	</div>
+</div> -->
+
+<style>
+	
+	.flex-col-center {
+	width: 100%;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	flex-direction: column
+	}
+
+	#product-img-2 {
+		width: 100%;
+		height: 300px;
+		background-size: cover;
+		background-position: center;
+	}
+
+</style>
+
+<div class="container-fluid bg-gray-lightest">
+	<h2 class="txt-xs-center pt-30">Latests Footwear</h2>
+	<div class="container">
+		<div class="row py-3">
+			<div class="col-md-4 flex-col-center">
+			    <div id="product-img-2" class="" style="background-image: url(<?php echo $BlockIMG_3 ?>)">
+			    </div>
+			    <h2 class="h3-alt">Product title<?php echo $product; ?></h2>
+			    <p class="txt-xs-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat officiis maxime eveniet ut</p>
+			</div>
+			<div class="col-md-4 flex-col-center">
+			    <div id="product-img-2" class="" style="background-image: url(<?php echo $BlockIMG_3 ?>)">
+			    </div>
+			    <h2 class="h3-alt">Product title<?php echo $product; ?></h2>
+			    <p class="txt-xs-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, numquam, voluptas. Velit qu</p>
+			</div>
+			<div class="col-md-4 flex-col-center">
+			    <div id="product-img-2" class="" style="background-image: url(<?php echo $BlockIMG_3 ?>)">
+			    </div>
+			    <h2 class="h3-alt">Product title<?php echo $product; ?></h2>
+			    <p class="txt-xs-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, numquam, voluptas. Velit qu</p>
+			</div>
+		</div>
+	</div>
+</div>
+
+
+
+
+
+
+<?php 
+// Spacing	
+		// Add a class to hide the seperation
+		//$hide = "hidden-xs-down";
+
 		include (ROOT_PATH . 'INC/Spacing-mt-100.php');
+
+
 
 // Footer
 
